88de9076c0332a0351a26e37af8a3d7a5ca5c285
geekbim <manyuabim9@gmail.com>
2022-04-08 16:48:19 +0700

    feat(navigation): add custom navigation

--- admin/config.tsx
+++ admin/config.tsx
@@ -1,6 +1,8 @@
 import { AdminConfig } from '@keystone-6/core/types';
 import { CustomLogo } from './components/CustomLogo';
+import { CustomNavigation } from './components/CustomNavigation';
 
 export const components: AdminConfig['components'] = {
     Logo: CustomLogo,
+    Navigation: CustomNavigation,
 }
